95117f095ad9e25ba1afba8e300db2cf1937139f
oom083464-jpg <oom083464@gmail.com>
2026-05-15 02:46:56 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    with open("index.html", "r", encoding="utf-8") as file:
-        return render_template_string(file.read())
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
