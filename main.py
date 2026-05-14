d43117bd1ca502b987c25697e607388918b68eb3
oom083464-jpg <oom083464@gmail.com>
2026-05-15 02:45:24 +0630

    Modify home route to use render_template_string
    Updated home route to render HTML from index.html file.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Render!"
+    with open("index.html", "r", encoding="utf-8") as file:
+        return render_template_string(file.read())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
